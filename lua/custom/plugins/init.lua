00cddda5e147c98b19dac73a90b03b12b2621d7d
Gavin Hailey <gavin.hailey@ibotta.com>
2024-03-22 12:12:54 -0600

    plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,12 +2,5 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {
-  {
-    url = "https://github.com/github/copilot.vim.git",
-  },
-  {
-    url = "https://github.com/google/vim-jsonnet.git",
-  }
-}
+return {}
 
