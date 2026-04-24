df241329b9814c0fcf25a1f37684975a06659355
Gavin Hailey <gavin.hailey@ibotta.com>
2026-04-24 16:36:39 -0600

    updates

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,6 +1,8 @@
+vim.opt.diffopt = 'internal,filler,closeoff,indent-heuristic,linematch:60,algorithm:histogram'
+vim.opt.tabstop = 4
+
 -- You can add your own plugins here or in other files in this directory!
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
 return {}
-
